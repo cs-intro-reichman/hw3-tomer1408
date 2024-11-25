e6b0ec5422388da387326333cbb5e623498a21d6
tomer1408 <tomer.rozental@post.runi.ac.il>
2024-11-25 09:07:31 +0200

    homeWork3

--- Algebra.java
+++ Algebra.java
@@ -28,14 +28,17 @@ public class Algebra {
 	   //System.out.println(pow(0,5));  // 0^5 = 0
 	   //System.out.println(pow(1,5));  // 1^5 = 1
 	   //System.out.println(pow(5,0));  // 5^0 = 1
-   		 System.out.println(div(12,3));   // 12 / 3 = 4    
-   		System.out.println(div(5,5));    // 5 / 5   = 1
-   		System.out.println(div(25,7));   // 25 / 7 = 3.571
-		   System.out.println(div(0,7));   // 0 / 7 = 0
-		   System.out.println(div(25,0));   // 25 / 0 = 0
-		   System.out.println(div(25,25));   // 25 / 25 = 1
-   		// System.out.println(mod(25,7));   // 25 % 7
-   		// System.out.println(mod(120,6));  // 120 % 6    
+   		 //System.out.println(div(12,3));   // 12 / 3 = 4    
+   		//System.out.println(div(5,5));    // 5 / 5   = 1
+   		//System.out.println(div(25,7));   // 25 / 7 = 3.571
+		   //System.out.println(div(0,7));   // 0 / 7 = 0
+		   //System.out.println(div(25,0));   // 25 / 0 = 0
+		   //System.out.println(div(25,25));   // 25 / 25 = 1
+		   System.out.println(mod(0,3));  // 0 % 3 = 0  
+		   System.out.println(mod(20,1));  // 20 % 1 = 0  
+   		System.out.println(mod(20,5));   // 20 % 5 = 0
+   		System.out.println(mod(10,3));  // 10 % 3 = 1  
+		
    		// System.out.println(sqrt(36));
 		// System.out.println(sqrt(263169));
    		// System.out.println(sqrt(76123));
@@ -203,10 +206,30 @@ public static int pow(int x, int n) {
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}	
+	
+		if (x1 == 0) {
+			return 0; }
+		if (x2 == 1) {
+			return 0;
+		}
+			boolean isNegative = x1 < 0; 
+		if (x1 < 0) {
+			x1 = negativeToPoitive(x1); 
+		}
+		if (x2 < 0) {
+			x2 = negativeToPoitive(x2);
+		}
+	
+	
+		while (x1 >= x2) {
+			x1 = plus(x1, negativeToPoitive(x2)); 
+		}
+	
+	
+		return isNegative ? plus(0, negativeToPoitive(x1)) : x1;
 
+	}
+	
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
 		// Replace the following statement with your code

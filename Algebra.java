b8f9283e34b5e38eb391119e242aa64e8d27559f
tomer1408 <tomer.rozental@post.runi.ac.il>
2024-11-25 08:44:28 +0200

    homeWork3

--- Algebra.java
+++ Algebra.java
@@ -17,16 +17,23 @@ public class Algebra {
 		// System.out.println(minus((-7),(-2)));  // -7 - -2 = -5
 		// System.out.println(minus((-7),2));  // -7 - 2 = -9
 		// System.out.println(minus((7),(-2)));  // 7 - -2 = 9
- 		System.out.println(times(3,4));  // 3 * 4 = 12
-		 System.out.println(times((-3),(-4)));  // -3 * -4 = 12
-		 System.out.println(times((-3),4));  // -3 * 4 = -12
-		 System.out.println(times(3,(-4)));  // 3 *-4 = -12
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		// System.out.println(pow(5,3));      // 5^3
-   		// System.out.println(pow(3,5));      // 3^5
-   		// System.out.println(div(12,3));   // 12 / 3    
-   		// System.out.println(div(5,5));    // 5 / 5  
-   		// System.out.println(div(25,7));   // 25 / 7
+ 		//System.out.println(times(3,4));  // 3 * 4 = 12
+		 //System.out.println(times((-3),(-4)));  // -3 * -4 = 12
+		 //System.out.println(times((-3),4));  // -3 * 4 = -12
+		 //System.out.println(times(3,(-4)));  // 3 *-4 = -12
+   		//System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+   	//System.out.println(pow(5,3));      // 5^3 = 125
+	   //System.out.println(pow(-5,3));      // 5^3 = -125
+   	//System.out.println(pow(3,2));      // 3^2 = 9
+	   //System.out.println(pow(0,5));  // 0^5 = 0
+	   //System.out.println(pow(1,5));  // 1^5 = 1
+	   //System.out.println(pow(5,0));  // 5^0 = 1
+   		 System.out.println(div(12,3));   // 12 / 3 = 4    
+   		System.out.println(div(5,5));    // 5 / 5   = 1
+   		System.out.println(div(25,7));   // 25 / 7 = 3.571
+		   System.out.println(div(0,7));   // 0 / 7 = 0
+		   System.out.println(div(25,0));   // 25 / 0 = 0
+		   System.out.println(div(25,25));   // 25 / 25 = 1
    		// System.out.println(mod(25,7));   // 25 % 7
    		// System.out.println(mod(120,6));  // 120 % 6    
    		// System.out.println(sqrt(36));
@@ -131,6 +138,7 @@ boolean isPositive= true;
 		isPositive=!isPositive;
 	}
 	for(int i = 1; i < x2; i++){
+		
 		multiplier=plus(multiplier, x1);
 	}if (isPositive) {
 		return multiplier;
@@ -142,16 +150,55 @@ boolean isPositive= true;
 
 	
 
-	// Returns x^n (for n >= 0)
-	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
-	}
+// Returns x^n (for n >= 0)
+public static int pow(int x, int n) {
+
+    if (x == 0) { 
+        return 0; 
+    }
+    if (n == 0) { 
+        return 1; 
+    }
+    if (x == 1) { 
+        return 1; 
+    }
+    int holding = 1; 
+    for (int i = 0; i < n; i++) {
+        holding = times(holding, x); 
+    }
+
+    return holding; 
+}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result= 0;
+		boolean isNegative = false;
+    if (x1 == 0 || x2 == 0) {
+        return 0; 
+    }
+	if (x2 == 1) {
+        return 0; 
+    }
+	if (x1 == x2)  {
+        return 1; 
+    }
+	
+    if (x1 < 0) {
+        x1 = negativeToPoitive(x1); 
+        isNegative = !isNegative; 
+    }
+    if (x2 < 0) {
+        x2 = negativeToPoitive(x2); 
+        isNegative = !isNegative;   
+    }
+
+    while (x1 >= x2) {
+        x1 = plus(x1, negativeToPoitive(x2)); 
+        result = plus(result, 1);         
+    }
+    return isNegative ? plus(0, negativeToPoitive(result)) : result;
+	
 	}
 
 	// Returns x1 % x2
